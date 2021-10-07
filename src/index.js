2f93bf813fde9f46bd15db70f7c296b283a88074
mcakeyerima <mcakyerima@gmail.com>
2021-10-07 23:38:01 +0100

    Nav and Menu items

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import React from 'react'; 
 import ReactDOM from 'react-dom';
 import App from './App';
+import { BrowserRouter as Router } from 'react-router-dom';
 
-
-ReactDOM.render(<App/> , document.getElementById('root'));
+ReactDOM.render(
+    // we hug our App with Router in order to use <Link/> of react-router-dom
+    <Router>
+        <App/>
+    </Router> ,
+     document.getElementById('root'));
