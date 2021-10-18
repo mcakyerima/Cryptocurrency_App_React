6576bad1bc7d179be475a0e2e15cb6a55ce7862d
mcakeyerima <mcakyerima@gmail.com>
2021-10-18 23:52:19 +0100

    Creating News Component and connecting to News Api

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Select , Typography , Row, Avatar, Card } from 'antd';
+import { Select , Typography , Row, Avatar, Card, Col } from 'antd';
 import { useGetCryptoNewsQuery } from '../services/cryptoNewsApi';
 import moment from 'moment';
 
@@ -7,13 +7,25 @@ const { Text , Title} = Typography;
 const { Options } = Select;
 const News = ({ simplified }) => {
     // pull out data from
-    const { data: cryptoNews } = useGetCryptoNewsQuery({ Cryptocurrency: 'newsCategory', count: simplified ? 6 : 12 });
+    const { data: cryptoNews } = useGetCryptoNewsQuery({ newsCategory: 'Cryptocurrency', count: simplified ? 6 : 12 });
     if(!cryptoNews) return "Loading News";
     console.log(cryptoNews)
+    if(!cryptoNews.value) return 'Loading';
     return (
-        <div>
-            News
-        </div>
+        <Row gutter={[24 , 24]}>
+            {cryptoNews.value.map((news , key) => (
+                <Col xs={24} sm={12}  lg={8} key={key}>
+                    <Card hoverable className="news-card">
+                        <a href={news.url} target="_blank" rel='noferrer'>
+                            <div className="news-image-container">
+                                <Title className="news-title" level={4}>{news.name}</Title>
+                            </div>
+                        </a>
+                    </Card>
+                </Col>
+            ))}
+
+        </Row>
     )
 }
 
